406ea272457c794625ba853971ebb046a0c5d95e
Ahsan Habib <smahsanhabib1999@gmail.com>
2024-12-16 20:09:32 +0600

    first commit

---
+++ src/habib/leapyear.java
@@ -0,0 +1,16 @@
+package habib;
+import java.util.Scanner;
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Year :");
+        int year = sc.nextInt();
+        if ( year%100==0 && year%400==0|| year%100!=0 && year % 4 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
+
+
