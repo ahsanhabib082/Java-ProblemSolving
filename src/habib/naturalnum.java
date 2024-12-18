2d2fbdac6cead89cdb5184b7f640538d1761a119
Ahsan Habib <smahsanhabib1999@gmail.com>
2024-12-18 21:27:40 +0600

    first commit

---
+++ src/habib/naturalnum.java
@@ -0,0 +1,16 @@
+package habib;
+
+import java.util.Scanner;
+
+public class naturalnum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number N :");
+        int n = sc.nextInt();
+        int sum;
+        sum=n*(n+1)/2;
+        System.out.println(sum);
+
+
+    }
+}
